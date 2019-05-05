328a4ca653ffebd0ad83e66bb99f54e05f4156cf
KoishiChan <qwq@koishi.top>
2019-05-06 00:12:11 +0800

    builds on windows

--- build.rs
+++ build.rs
@@ -18,14 +18,17 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", llvm_lib_dir);
 
     let output = Command::new("llvm-config")
-        .args(&["--libs"])
+        .args(&["--libnames"])
         .output()
         .unwrap();
 
     let llvm_libs = String::from_utf8_lossy(&output.stdout);
     let llvm_libs: Vec<_> = llvm_libs
         .split(" ")
-        .map(|x| x.trim_start_matches("-l"))
+        .map(|x| x.trim()
+            .trim_start_matches("lib")
+            .trim_end_matches(".lib")
+            .trim_end_matches(".a"))
         .collect();
 
     println!("cargo:rustc-link-search=native={}", dst.display());
@@ -44,12 +47,12 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     if target.contains("apple") {
         println!("cargo:rustc-link-lib=dylib=c++");
-    } else 
-    // if target.contains("linux") 
-    {
+    } else if target.contains("linux") {
         println!("cargo:rustc-link-lib=dylib=stdc++");
-    // } else {
-    //     println!("cargo:warning={}", "windows is not a supported platform.");
-    //     unimplemented!();
+    } else if target.contains("windows") {
+        println!("cargo:rustc-link-lib=dylib=msvcrt");
+    } else {
+        println!("cargo:warning={} is not a supported platform.", target);
+        unimplemented!();
     }
 }
