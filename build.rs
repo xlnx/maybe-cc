c486311a070c0c3d0ff56620b6d0b194aec932ad
KoishiChan <qwq@koishi.top>
2019-05-05 16:13:24 +0800

    fix windows build

--- build.rs
+++ build.rs
@@ -6,22 +6,28 @@ use std::process::Command;
 fn main() {
     let dst = cmake::build("ir-gen");
 
-    Command::new("llvm-config")
-        .args(&["--libs"])
-        .status()
-        .unwrap();
+    let output = Command::new("llvm-config")
+        .args(&["--libdir"])
+        .output()
+        .unwrap_or_else(|_| {
+            println!("cargo:warning={}", "llvm-config not in PATH");
+            std::process::exit(0);
+        });
+    
+    println!("cargo:rustc-link-search=native={}", String::from_utf8_lossy(&output.stdout));
 
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=ir-gen");
 
     println!("cargo:rustc-link-lib=static=LLVMCore");
     println!("cargo:rustc-link-lib=static=LLVMSupport");
+
     let target = env::var("TARGET").unwrap();
     if target.contains("apple") {
         println!("cargo:rustc-link-lib=dylib=c++");
     } else if target.contains("linux") {
         println!("cargo:rustc-link-lib=dylib=stdc++");
-    } else {
-        unimplemented!();
+    // } else {
+    //     unimplemented!();
     }
 }
