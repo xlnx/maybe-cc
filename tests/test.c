3154d09f89e530cbc58a20da0e0e782f2dc7f7cf
KoishiChan <qwq@koishi.top>
2019-05-08 21:04:46 +0800

    tree print

--- tests/test.c
+++ tests/test.c
@@ -1,12 +1,6 @@
-# 1 "test.c"
-# 1 "<built-in>"
-# 1 "<command-line>"
-# 31 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 32 "<command-line>" 2
-# 1 "test.c"
+
+
 int main()
 {
-	int a;
-	a += b[] + 128 * 4;
+	struct A c;
 }
