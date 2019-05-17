4dd52d879ca4f6c45f4212fb3c421153919c9ff6
KoishiChan <qwq@koishi.top>
2019-05-17 18:54:14 +0800

    rm maybe.sh

--- maybe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CC="./mcc"
-
-gcc -E -std=c89 -U__GNUC__ -U__GNUC_MINOR__ -U__GNUC_PACKLEVEL__ $1 | \
-    $CC -o ./a.ll && \
-    llc ./a.ll --filetype=obj -o ./a.o && \
-    gcc ./a.o -lm
