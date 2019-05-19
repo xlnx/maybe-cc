d679f9875e048c37b31eb58974cc2723336dcd48
KoishiChan <qwq@koishi.top>
2019-05-19 23:50:12 +0800

    fix pointer typegen order

--- tests/warnings/cast_away_qualifiers.c
+++ tests/warnings/cast_away_qualifiers.c
@@ -9,6 +9,9 @@ int main()
 	const arr_t a; /* const int a[10]; */
 	int *r = &a[ 1 ];
 
-	const cint_ptr_t *vals;		/* const int *const *vals */
-	int *const *val_ptr = vals; /*       int *const *     */
+	const cint_ptr_t *val_p;	   /* const int *const *vals   */
+	int *const *val_ptr_p = val_p; /*       int *const *       */
+
+	const cint_ptr_t val_a[ 2 ];   /* const int *const vals[2] */
+	int *const *val_ptr_a = val_a; /*       int *const *       */
 }
